44b1b8ed770bac8a69683db3b2e2c777d71453ad
Max <maksim@sadovskij.com>
2019-07-17 08:47:24 +0300

    Log only when DEBUG env

--- src/common/bot/functions.rb
+++ src/common/bot/functions.rb
@@ -17,7 +17,9 @@ module Bot
 end
 
 def putsd(*args)
-    puts args
+    if ENV['DEBUG'] == '1'
+        puts args
+    end
 end
 
 OPERA_USERAGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'

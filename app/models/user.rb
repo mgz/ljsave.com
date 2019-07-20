755fb5fea0be707081f8708a71d800f84a1d4c38
Max <maksim@sadovskij.com>
2019-07-20 13:20:50 +0300

    Do not use subdomains

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     
     def self.get_root_url(username, request:)
-        if Rails.env.production?
-            return "//#{username}.#{request.host_with_port}"
-        else
+        # if Rails.env.production?
+        #     return "//#{username}.#{request.host_with_port}"
+        # else
             return "//#{request.host_with_port}/user/#{username}"
-        end
+        # end
     end
 end

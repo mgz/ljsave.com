438d36666c4faf89c6a45cbc4aeff5a0c4159194
Max <maksim@sadovskij.com>
2019-07-22 15:19:47 +0300

    .

--- app/controllers/includes_controller.rb
+++ app/controllers/includes_controller.rb
@@ -1,11 +1,10 @@
 class IncludesController < ApplicationController
   
   def body
-    render plain: params.inspect
-    # @username = params[:uri] ? params[:uri][%r{/lj/(.+?)/}, 1] : 'dummy'
-    # @iframe_html = render_to_string(partial: '/includes/body/html', locals: {username: @username}).to_sym.to_s
-    #
-    # render layout: nil
+    @username = params[:uri] ? params[:uri][%r{/lj/(.+?)/}, 1] : 'dummy'
+    @iframe_html = render_to_string(partial: '/includes/body/html', locals: {username: @username}).to_sym.to_s
+
+    render layout: nil
   end
   
   def head

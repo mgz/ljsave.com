a9557ee5119c79f3414781e7138ebaee416b5ede
Max <maksim@sadovskij.com>
2019-08-16 10:23:33 +0300

    Extract ExpandLink class

--- app/services/downloader/comment_expander.rb
+++ app/services/downloader/comment_expander.rb
@@ -1,48 +1,8 @@
 require "awesome_print"
 
 class CommentExpander
-  def self.get_expand_links(browser)
-    putsd 'Finding expand links...'
-    browser.manage.timeouts.implicit_wait = 0
-    expand_links = browser.find_elements(css: '.b-leaf-footer .b-leaf-actions-expandchilds a')
-    expand_links += browser.find_elements(css: '.b-leaf-collapsed .b-leaf-header .b-leaf-actions-expand a')
-    expand_links += browser.find_elements(css: '.b-leaf-seemore-expand a')
-    
-    putsd "    Found #{expand_links.size} links"
-    
-    links_at_depths = {}
-    
-    expand_links.each do |link|
-      begin
-        parent = link.find_element(xpath: '../../../../../..')
-        if parent.attribute('style').present?
-          margin_left = parent.style('margin-left')
-        else
-          margin_left = link.find_element(xpath: '../../../../../../..').style('margin-left')
-        end
-        
-        depth = margin_left[/(\d+)/, 1].to_i / 30
-        links_at_depths[depth] ||= []
-        links_at_depths[depth] << link
-      rescue Selenium::WebDriver::Error::StaleElementReferenceError
-        # Link is gone
-      end
-    end
-    return links_at_depths[links_at_depths.keys.min] || []
-  end
-  
   def self.expand_all_comments_on_page(browser)
-    slept_seconds = 0.0
-    delta = 0.5
-    while browser.find_elements(css: '#comments.b-grove-loading').any? || browser.find_elements(css: 'div.b-grove.b-grove-hover').any?
-      putsd 'Still have preloader, waiting'
-      slept_seconds += delta
-      sleep delta
-      if slept_seconds > 5
-        browser.execute_script('window.location.reload()')
-      end
-      break if slept_seconds > 10
-    end
+    wait_for_page_to_load(browser)
     
     links_clicked_times = Hash.new(0)
     
@@ -53,16 +13,14 @@ class CommentExpander
       
       links.each do |link|
         begin
-          href = link.attribute('href')
-          if links_clicked_times[href] > 5
-            putsd "    Clicked #{href} too many times, exiting..."
+          if links_clicked_times[link.href] > 5
+            putsd "    Clicked #{link.href} too many times, exiting..."
             # sleep 1000
             return browser.page_source
           end
-          putsd "    Clicking #{href}..."
-          # link.click
-          browser.execute_script("arguments[0].click();", link)
-          links_clicked_times[href] += 1
+          putsd "    Clicking #{link.href}..."
+          link.click
+          links_clicked_times[link.href] += 1
           sleep 1
         rescue Selenium::WebDriver::Error::ElementClickInterceptedError
           putsd '    Click intercepted'
@@ -73,38 +31,37 @@ class CommentExpander
           putsd '    Element not interactable'
         end
       end
-      
-      
       sleep 1
-      
-      # Parallel.each(links, in_threads: 6) do |a|
-      #     # putsd span.attribute('style')
-      #     # a = span.find_element(css: 'a')
-      #     # comment_id = a.attribute('onclick')[/'(\d+)'/, 1]
-      #     putsd "Expanding ..."
-      #     begin
-      #         a.click
-      #         # browser.execute_script("arguments[0].click();", a)
-      #
-      #         times_slept = 0
-      #         while a.displayed?
-      #             sleep 0.3
-      #             times_slept += 1
-      #             raise 'Sleeping too long' if times_slept > 10
-      #         end
-      #     rescue Selenium::WebDriver::Error::ElementClickInterceptedError
-      #         putsd '    Click intercepted'
-      #     rescue Selenium::WebDriver::Error::StaleElementReferenceError
-      #         # Element is gone, fine.
-      #         putsd '    Element gone'
-      #     # rescue Errno::EPIPE, EOFError => e
-      #     #     putsd e
-      #     end
-      #     # putsd ' done.'
-      # end
     end
-    
     putsd '    * Everything expanded'
     return browser.page_source
   end
+  
+  private
+  def self.get_expand_links(browser)
+    putsd 'Finding expand links...'
+    links = ExpandLink.find_on_page(browser)
+    putsd "    Found #{links.size} links"
+    links_at_depths = ExpandLink.arrange_by_depth(links)
+    return links_at_depths[links_at_depths.keys.min] || []
+  end
+  
+  def self.wait_for_page_to_load(browser)
+    slept_seconds = 0.0
+    delta = 0.5
+    while page_still_loading?(browser)
+      putsd 'Still have preloader, waiting'
+      slept_seconds += delta
+      sleep delta
+      if slept_seconds > 5
+        browser.execute_script('window.location.reload()')
+      end
+      break if slept_seconds > 10
+    end
+  end
+  
+  def self.page_still_loading?(browser)
+    return browser.find_elements(css: '#comments.b-grove-loading').any? || browser.find_elements(css: 'div.b-grove.b-grove-hover').any?
+  end
+  
 end
---
+++ app/services/downloader/expand_link.rb
@@ -0,0 +1,53 @@
+class ExpandLink
+  def initialize(html_element, browser)
+    @elem = html_element
+    @browser = browser
+  end
+
+  def self.find_on_page(browser)
+    browser.manage.timeouts.implicit_wait = 0
+    expand_links = browser.find_elements(css: '.b-leaf-footer .b-leaf-actions-expandchilds a')
+    expand_links += browser.find_elements(css: '.b-leaf-collapsed .b-leaf-header .b-leaf-actions-expand a')
+    expand_links += browser.find_elements(css: '.b-leaf-seemore-expand a')
+    return expand_links.map{|e| ExpandLink.new(e, browser)}
+  end
+  
+  def click
+    @browser.execute_script("arguments[0].click();", @elem)
+  end
+  
+  def href
+    @href ||= @elem.attribute('href')
+  end
+
+  def self.arrange_by_depth(links)
+    links_at_depths = {}
+    links.each do |link|
+      if (depth = link.depth)
+        links_at_depths[depth] ||= []
+        links_at_depths[depth] << link
+      end
+    end
+    return links_at_depths
+  end
+
+  def depth
+    begin
+      depth = margin_left[/(\d+)/, 1].to_i / 30
+      return depth
+    rescue Selenium::WebDriver::Error::StaleElementReferenceError
+      # Link is gone
+      return nil
+    end
+  end
+
+  def margin_left
+    parent = @elem.find_element(xpath: '../../../../../..')
+    if parent.attribute('style').present?
+      margin_left = parent.style('margin-left')
+    else
+      margin_left = @elem.find_element(xpath: '../../../../../../..').style('margin-left')
+    end
+    return margin_left
+  end
+end

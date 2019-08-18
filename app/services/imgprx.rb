b73514effd93c0d3310706357c0465a7bd9d0cad
Max <maksim@sadovskij.com>
2019-08-18 16:18:00 +0300

    Cleanup

--- app/services/imgprx.rb
+++
@@ -1,7 +0,0 @@
-class Imgprx
-  def self.fix
-    puts 'Searching...'
-    files_to_fix = `grep -l --include='[0123456789]*.html' -r . -e 'https://imgprx.livejournal.net'`
-    puts "Found #{files_to_fix.size} files to fix"
-  end
-end

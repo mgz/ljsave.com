14a4d1598bdb8c5e85ae19f4a11c81635f1f9958
Max <maksim@sadovskij.com>
2019-08-21 12:26:06 +0300

    Refactor test

--- spec/models/expand_link_spec.rb
+++ spec/models/expand_link_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
+def load_palaman_file
+  file_path = 'spec/html_data/palaman_362622.html'
+  browser.get("file://#{File.absolute_path(file_path)}")
+end
+
 RSpec.describe Downloader::ExpandLink do
   let(:browser) {Downloader::Chrome.create(headless: true)}
   
   it "finds ExpandLinks on page" do
-    file_path = 'spec/html_data/palaman_362622.html'
-    browser.get("file://#{File.absolute_path(file_path)}")
+    load_palaman_file
     
     found_links = Downloader::ExpandLink.find_on_page(browser)
     expect(found_links.count).to eql(32)
 
     top_level_links = Downloader::ExpandLink.top_level_links(found_links)
     expect(top_level_links.count).to eql(6)
-    
-    
   end
 end

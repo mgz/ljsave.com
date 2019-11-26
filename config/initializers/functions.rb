0ab267fc90846ba6f37a027de620eacd97a3f406
Max <maksim@sadovskij.com>
2019-11-26 13:01:48 +0200

    Include open-uri

--- config/initializers/functions.rb
+++ config/initializers/functions.rb
@@ -7,6 +7,7 @@ end
 OPERA_USERAGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'
 
 def read_url(url, opts: {})
+  require 'open-uri'
   if (proxy = ENV['PROXY'])
     proxy = "http://#{proxy}" unless proxy.start_with?('http://')
     opts[:proxy] = URI.parse(proxy)
